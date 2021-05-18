e5df99f0ab0ff39bef763c937afc07fa3f32ce02
Jonathan Pallant (42 Technology) <jonathan.pallant@42technology.com>
2021-05-18 17:39:17 +0100

    Clean up build scripts.

--- debug.sh
+++ debug.sh
@@ -2,15 +2,24 @@
 set -euo pipefail
 
 OUTPUT_DIR=./target/bin/debug
-INPUT=./target/thumbv8m.main-none-eabi/debug/nrf9160-demo
+INPUT=./target/thumbv8m.main-none-eabihf/debug/nrf9160-demo
+
+function process {
+    local build_type=$1
+    echo "Creating $1.elf..."
+    cargo build --features=$1
+    cp $INPUT $OUTPUT_DIR/$1.elf
+    echo "Creating $1.hex..."
+    arm-none-eabi-objcopy -O ihex $INPUT $OUTPUT_DIR/$1.hex
+    echo "Creating $1.sym..."
+    arm-none-eabi-objdump -t $INPUT > $OUTPUT_DIR/$1.sym
+    echo "Creating $1.S..."
+    arm-none-eabi-objdump -dC $INPUT > $OUTPUT_DIR/$1.S
+    echo "Creating $1.bin..."
+    arm-none-eabi-objcopy -O binary $INPUT $OUTPUT_DIR/$1.bin
+}
+
 mkdir -p $OUTPUT_DIR
-cargo build --features=nrf9160dk
-arm-none-eabi-objcopy -O ihex $INPUT $OUTPUT_DIR/nrf9160dk.hex
-arm-none-eabi-objdump -t $INPUT > $OUTPUT_DIR/nrf9160dk.sym
-arm-none-eabi-objdump -dC $INPUT > $OUTPUT_DIR/nrf9160dk.S
-arm-none-eabi-objcopy -O binary $INPUT $OUTPUT_DIR/nrf9160dk.bin
-cargo build --features=icarus
-arm-none-eabi-objcopy -O ihex $INPUT $OUTPUT_DIR/icarus.hex
-arm-none-eabi-objdump -t $INPUT > $OUTPUT_DIR/icarus.sym
-arm-none-eabi-objdump -dC $INPUT > $OUTPUT_DIR/icarus.S
-arm-none-eabi-objcopy -O binary $INPUT $OUTPUT_DIR/icarus.bin
+process nrf9160dk
+process icarus
+ls -lh $OUTPUT_DIR
